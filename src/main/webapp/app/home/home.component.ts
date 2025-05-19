a168be9d499575876a18bcc1bf12f5ba19f656f0
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-05-19 12:02:35 +0100

    feat(frontend): Update home page to display content based on authentication status

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from 'environments/environment';
 import { RouterLink } from '@angular/router';
-
+import { AuthService } from '../services/auth.service';
+import { UserRole } from '../models/auth.model';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
   environment = environment;
+  isLoggedIn = false;
+  username = '';
+  userRole = '';
+  userRoles = UserRole;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe(user => {
+      this.isLoggedIn = !!user;
+      if (user) {
+        this.username = user.username;
+        this.userRole = user.role;
+      }
+    });
+  }
+
+  isAdmin(): boolean {
+    return this.authService.isAdmin();
+  }
+
+  isEmploye(): boolean {
+    return this.authService.isEmploye();
+  }
+
+  isClient(): boolean {
+    return this.authService.isClient();
+  }
 }
