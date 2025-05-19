a798bebcfea450a73306427f15530e1237c24af4
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-05-19 09:03:03 +0100

    template from bootify

---
+++ src/main/java/io/bootify/exam_springboot_angular/ExamSpringbootAngularApplication.java
@@ -0,0 +1,14 @@
+package io.bootify.exam_springboot_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ExamSpringbootAngularApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ExamSpringbootAngularApplication.class, args);
+    }
+
+}
